0effd99a34cc2d00734395b813d7ecb607332dd5
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-19 20:44:38 +0900

    [add]InputBufferを追加

---
+++ Assets/Scripts/Runtime/System/InputBuffer.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace KillHouse.Runtime.System
+{
+    public class InputBuffer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
