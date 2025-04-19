c355f33b59e97dbe4c22a87a5d961616d5aadcc1
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-19 19:16:52 +0900

    [update]3D用にプロジェクトを改装

---
+++ Assets/Scripts/SymphonyFrameWork/Enum/TagsEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+[Flags]
+public enum TagsEnum : int
+{
+    None = 1 << 0,
+    Untagged = 1 << 1,
+    Respawn = 1 << 2,
+    Finish = 1 << 3,
+    EditorOnly = 1 << 4,
+    MainCamera = 1 << 5,
+    Player = 1 << 6,
+    GameController = 1 << 7,
+    Gimmick = 1 << 8,
+}
