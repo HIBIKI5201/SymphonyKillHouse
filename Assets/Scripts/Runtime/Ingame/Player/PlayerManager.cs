af73f329631039b5807b77191c64e060b5813e7a
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-20 23:47:54 +0900

    [update]入力を受け取るシステムを実装

--- Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
+++ Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
@@ -1,13 +1,18 @@
 using System;
+using KillHouse.Runtime.System;
+using SymphonyFrameWork.System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace KillHouse.Runtime.Ingame
 { 
     public class PlayerManager : MonoBehaviour 
     {
-        private void Update()
+        private void Start()
         {
-            
+            var inputBuffer = ServiceLocator.GetInstance<InputBuffer>();
+            var actions = inputBuffer.PlayerInput.actions;
+            actions["Attack"].started += ctx => Debug.Log("Attack");
         }
     } 
 }
