8934a257c42065cf0679d15589d02c9a4841ff84
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-20 23:35:05 +0900

    [update]InputBufferにPlayerInputの取得処理を追加

--- Assets/Scripts/Runtime/System/InputBuffer.cs
+++ Assets/Scripts/Runtime/System/InputBuffer.cs
@@ -1,13 +1,21 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using SymphonyFrameWork.System;
 
 namespace KillHouse.Runtime.System
 {
+    /// <summary>
+    /// 入力を受け取るシステム
+    /// </summary>
+    [RequireComponent(typeof(PlayerInput))]
     public class InputBuffer : MonoBehaviour
     {
+        private PlayerInput _playerInput;
+        
         private void Awake()
         {
-            
+            _playerInput = GetComponent<PlayerInput>();
         }
     }
 }
