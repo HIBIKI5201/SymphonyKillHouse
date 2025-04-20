fbb07783f8da64b126613356618c9425d8bbe705
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-20 23:40:52 +0900

    [update]アクションマップの変更メソッドを追加

--- Assets/Scripts/Runtime/System/InputBuffer.cs
+++ Assets/Scripts/Runtime/System/InputBuffer.cs
@@ -1,7 +1,5 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using SymphonyFrameWork.System;
 
 namespace KillHouse.Runtime.System
 {
@@ -12,10 +10,33 @@ namespace KillHouse.Runtime.System
     public class InputBuffer : MonoBehaviour
     {
         private PlayerInput _playerInput;
+        public PlayerInput PlayerInput => _playerInput;
         
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
+        }
+
+        /// <summary>
+        ///     アクションマップを変更する
+        /// </summary>
+        /// <param name="kind"></param>
+        public void ChangeActionMap(ActionMapEnum kind)
+        {
+            if (kind == ActionMapEnum.None)
+            {
+                Debug.LogWarning("No action map defined");
+                return;
+            }
+            
+            _playerInput.SwitchCurrentActionMap(kind.ToString());
+        }
+
+        public enum ActionMapEnum
+        {
+            None = 0,
+            Player = 1,
+            UI = 2,
         }
     }
 }
