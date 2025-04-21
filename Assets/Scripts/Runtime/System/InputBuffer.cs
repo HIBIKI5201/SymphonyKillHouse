ad53cd97f486fe168f85709cf9cda02dec064ee5
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-21 23:10:57 +0900

    [update]InputActionContextクラスを追加し、入力をコンテキストに変換

--- Assets/Scripts/Runtime/System/InputBuffer.cs
+++ Assets/Scripts/Runtime/System/InputBuffer.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,11 +11,42 @@ namespace KillHouse.Runtime.System
     public class InputBuffer : MonoBehaviour
     {
         private PlayerInput _playerInput;
-        public PlayerInput PlayerInput => _playerInput;
+
+        public class InputActionContext : IDisposable
+        {
+            private InputAction _action;
+            public InputAction Action => _action;
+            public InputActionContext(InputAction action)
+            {
+                _action = action;
+                _action.started += ActionInvoke;
+                _action.performed += ActionInvoke;
+                _action.canceled += ActionInvoke;
+            }
+
+            public void Dispose()
+            {
+                _action.started -= ActionInvoke;
+                _action.performed -= ActionInvoke;
+                _action.canceled -= ActionInvoke;
+            }
+
+            private void ActionInvoke(InputAction.CallbackContext context) => Invoked?.Invoke(context);
+
+            public Action<InputAction.CallbackContext> Invoked;
+        }
+        
+        public InputActionContext Move => _move;
+        private InputActionContext _move;
+
+        public InputActionContext Attack => _attack;
+        private InputActionContext _attack;
         
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
+            _move = new InputActionContext( _playerInput.actions["Move"]);
+            _attack = new InputActionContext( _playerInput.actions["Attack"]);
         }
 
         /// <summary>
