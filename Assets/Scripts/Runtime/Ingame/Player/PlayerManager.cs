a3d1a56891c600152aa11c102625312c14b2e4b5
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-21 17:58:40 +0900

    [update]移動のアニメーションを設置

--- Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
+++ Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
@@ -3,16 +3,37 @@ using KillHouse.Runtime.System;
 using SymphonyFrameWork.System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace KillHouse.Runtime.Ingame
 { 
     public class PlayerManager : MonoBehaviour 
     {
+        private Animator _animator;
+        private Vector2 _moveInput = Vector2.zero;
+        
         private void Start()
         {
             var inputBuffer = ServiceLocator.GetInstance<InputBuffer>();
             var actions = inputBuffer.PlayerInput.actions;
             actions["Attack"].started += ctx => Debug.Log("Attack");
+            
+            var moveAction = actions["Move"];
+            moveAction.started += Move;
+            moveAction.performed += Move;
+            moveAction.canceled += Move;
+            
+            _animator = GetComponentInChildren<Animator>();
+        }
+
+        private void Move(InputAction.CallbackContext context)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+            if (_animator)
+            {
+                _animator.SetFloat("MoveX", _moveInput.x);
+                _animator.SetFloat("MoveY", _moveInput.y);
+            }
         }
     } 
 }
