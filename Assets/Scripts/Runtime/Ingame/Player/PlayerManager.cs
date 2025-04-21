eb59335e5900b9a755e2b65735af10528405aef6
HIBIKI_5201 <takuto5201@gmail.com>
2025-04-21 23:13:53 +0900

    [update]NavMeshを利用して移動できるように

--- Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
+++ Assets/Scripts/Runtime/Ingame/Player/PlayerManager.cs
@@ -1,39 +1,81 @@
-using System;
 using KillHouse.Runtime.System;
 using SymphonyFrameWork.System;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 namespace KillHouse.Runtime.Ingame
-{ 
-    public class PlayerManager : MonoBehaviour 
+{
+    public class PlayerManager : MonoBehaviour
     {
+        [SerializeField] private float _moveSpeed = 5f;
+
         private Animator _animator;
+        private static readonly int MoveX = Animator.StringToHash("MoveX");
+        private static readonly int MoveY = Animator.StringToHash("MoveY");
+
+        private bool _isMove;
         private Vector2 _moveInput = Vector2.zero;
-        
+
         private void Start()
         {
+            # region 入力系
+
             var inputBuffer = ServiceLocator.GetInstance<InputBuffer>();
-            var actions = inputBuffer.PlayerInput.actions;
-            actions["Attack"].started += ctx => Debug.Log("Attack");
-            
-            var moveAction = actions["Move"];
-            moveAction.started += Move;
-            moveAction.performed += Move;
-            moveAction.canceled += Move;
-            
+
+            //攻撃入力
+            inputBuffer.Attack.Action.started += _ => Debug.Log("Attack");
+
+            //移動入力
+            inputBuffer.Move.Invoked += OnMove;
+
+            #endregion
+
             _animator = GetComponentInChildren<Animator>();
         }
 
-        private void Move(InputAction.CallbackContext context)
+        private void Update()
         {
-            _moveInput = context.ReadValue<Vector2>();
-            if (_animator)
+            Move();
+        }
+
+        private void Move()
+        {
+            if (_isMove)
             {
-                _animator.SetFloat("MoveX", _moveInput.x);
-                _animator.SetFloat("MoveY", _moveInput.y);
+                //NavMeshから移動場所を選定
+                if (NavMesh.SamplePosition(transform.position
+                                           + new Vector3(_moveInput.x, 0, _moveInput.y) * (_moveSpeed * Time.deltaTime),
+                        out var hit, 1.0f, NavMesh.AllAreas))
+                    transform.position = hit.position;
             }
         }
-    } 
+
+        /// <summary>
+        /// 入力を受け取り
+        /// </summary>
+        /// <param name="context"></param>
+        private void OnMove(InputAction.CallbackContext context)
+        {
+            _moveInput = context.ReadValue<Vector2>();
+
+            switch (context.phase)
+            {
+                case InputActionPhase.Started:
+                    _isMove = true;
+                    break;
+
+                case InputActionPhase.Canceled:
+                    _isMove = false;
+                    break;
+            }
+
+            //アニメーションにパラメータを代入
+            if (_animator)
+            {
+                _animator.SetFloat(MoveX, _moveInput.x);
+                _animator.SetFloat(MoveY, _moveInput.y);
+            }
+        }
+    }
 }
